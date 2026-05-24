38616b7661244fec541bb80930d02b9f696fef89
Sayan Dubey <sayandubey25@gmail.com>
2026-05-24 11:24:10 +0100

    rock paper scissors commit

---
+++ main2.py
@@ -0,0 +1,30 @@
+import random
+player=int(input("Welcome to Rock Paper Scissors. Enter 1 for rock, 2 for paper or 3 for scissors."))
+comp=random.randint(1, 3)
+if player==1:
+    print("You choose rock")
+elif player==2:
+    print("You choose paper")
+elif player==3:
+    print("You choose scissors")
+else:
+    print("Not a valid choice")
+    #end code
+    
+if comp==1:
+    print("The opponent chose rock")
+elif comp==2:
+    print("The opponent chose paper")
+elif comp==3:
+    print("The opponent chose scissors")
+    
+if player==comp:
+    print("It is a tie.")
+elif comp==player+1:
+    print("You lose")
+elif comp==1 and player==3:
+    print("You lose")
+elif comp+1==player:
+    print("You win")
+elif comp==3 and player==1:
+    print("You win")
